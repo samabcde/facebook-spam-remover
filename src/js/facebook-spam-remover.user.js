25c9e00d2942d2eb220eace7cac536f907848d70
samabcde <tszsumng90@gmail.com>
2024-03-01 23:55:53 +0800

    initial commit

---
+++ src/js/facebook-spam-remover.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         Remove Span
+// @namespace    http://tampermonkey.net/
+// @version      0.0.1
+// @description  Removes Facebook Spam
+// @author       Samabcde
+// @match        https://www.facebook.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=facebook.com
+// @updateURL    TODO
+// @downloadURL  TODO
+// @grant        none
+// ==/UserScript==
+(function () {
+    console.log("started")
+})
