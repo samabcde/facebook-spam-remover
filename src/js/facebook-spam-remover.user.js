ab3f5ac39aff58ce0c37075fb51ab837fddc3ead
samabcde <tszsumng90@gmail.com>
2024-03-09 11:55:21 +0800

    rename to be more searchable

--- src/js/facebook-spam-remover.user.js
+++ src/js/facebook-spam-remover.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         Remove Spam
+// @name         Remove Facebook Spam
 // @namespace    http://tampermonkey.net/
-// @version      0.3.1
+// @version      0.3.2
 // @description  Removes Facebook Spam
 // @author       Samabcde
 // @match        https://www.facebook.com/*
